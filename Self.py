bb665a9a8f6f503a27d8e714f4580c55e11f5cd3
mazgutheng <35424920+mazgutheng@users.noreply.github.com>
2018-02-04 07:03:06 +0700

    Update Self.py

--- Self.py
+++ Self.py
@@ -10,6 +10,7 @@ import profile
 import client
 import wikipedia
 import requests
+from gtts import gTTS
 from CYBERTK.lib.curve.ttypes import *
 from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re
